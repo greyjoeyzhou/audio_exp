936c7f7b49d32443530239ab97575326f704d01a
joeyzhou1984 <joeyzhou1984@gmail.com>
2024-04-06 20:17:14 -0700

    setup for using hound for wave reading

--- python/audio_exp/__init__.py
+++ python/audio_exp/__init__.py
@@ -1,3 +1,3 @@
-from audio_exp._lowlevel import hello
+from audio_exp._lowlevel import hello, read_wav_file
 
-__all__ = ["hello"]
+__all__ = ["hello", "read_wav_file"]
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use hound;
 use pyo3::prelude::*;
 
 /// Prints a message.
@@ -6,9 +7,18 @@ fn hello() -> PyResult<String> {
     Ok("Hello from audio-exp!".into())
 }
 
+/// Reads a WAV file and returns the samples as a Vec<i16> wrapped with PyResult.
+#[pyfunction]
+fn read_wav_file(file_path: &str) -> PyResult<Vec<i16>> {
+    let mut reader = hound::WavReader::open(file_path).unwrap();
+    let samples = reader.samples::<i16>().map(|s| s.unwrap()).collect();
+    Ok(samples)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _lowlevel(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello, m)?)?;
+    m.add_function(wrap_pyfunction!(read_wav_file, m)?)?;
     Ok(())
 }
