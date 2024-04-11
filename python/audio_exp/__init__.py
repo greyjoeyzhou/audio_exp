7c1c64ceda49b2cb1db95f545a99b9cb433c4196
joeyzhou1984 <joeyzhou1984@gmail.com>
2024-04-11 00:48:05 -0700

    skeleton for testing, logging

--- python/audio_exp/__init__.py
+++ python/audio_exp/__init__.py
@@ -1,3 +1,9 @@
+import logging
+
 from audio_exp._lowlevel import hello, read_wav_file
+
+FORMAT = "%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s"
+logging.basicConfig(format=FORMAT)
+logging.getLogger().setLevel(logging.INFO)
 
 __all__ = ["hello", "read_wav_file"]
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,28 @@
+use log::info;
+
 use hound;
 use pyo3::prelude::*;
 
+// a general pattern would be implemented pure rust functions
+// then wrap as python functions by using pyo3's macros
+
+/// Returns a greeting.
+fn hello() -> String {
+    return "Hello from audio-exp!".to_string();
+}
+
 /// Prints a message.
-#[pyfunction]
-fn hello() -> PyResult<String> {
-    Ok("Hello from audio-exp!".into())
+#[pyfunction(name = "hello")]
+fn py_hello() -> PyResult<String> {
+    Ok(hello().into())
 }
 
 /// Reads a WAV file and returns the samples as a Vec<i16> wrapped with PyResult.
 #[pyfunction]
 fn read_wav_file(file_path: &str) -> PyResult<Vec<i16>> {
+    info!("Reading WAV file: {}", file_path);
     let mut reader = hound::WavReader::open(file_path).unwrap();
+    info!("reader created");
     let samples = reader.samples::<i16>().map(|s| s.unwrap()).collect();
     Ok(samples)
 }
@@ -18,7 +30,22 @@ fn read_wav_file(file_path: &str) -> PyResult<Vec<i16>> {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _lowlevel(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(hello, m)?)?;
+    pyo3_log::init();
+    m.add_function(wrap_pyfunction!(py_hello, m)?)?;
     m.add_function(wrap_pyfunction!(read_wav_file, m)?)?;
     Ok(())
+}
+
+// unittest could be written in the same rs file
+// we could test non-python functions in rs
+// for wrapped python functions, we should test them in python
+
+#[cfg(test)]
+mod tests {
+    use crate::hello;
+
+    #[test]
+    fn test_hello() {
+        assert_eq!(hello(), "Hello from audio-exp!");
+    }
 }
