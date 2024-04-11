e9187a7731fd5f10f92db9a5f9a15e2de022f2c7
joeyzhou1984 <joeyzhou1984@gmail.com>
2024-04-11 01:31:16 -0700

    read as numpy array

--- python/audio_exp/__init__.py
+++ python/audio_exp/__init__.py
@@ -1,9 +1,9 @@
-import logging
+# import logging
 
-from audio_exp._lowlevel import hello, read_wav_file
+from audio_exp._lowlevel import hello, read_wav_file, read_wav_file_np
 
-FORMAT = "%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s"
-logging.basicConfig(format=FORMAT)
-logging.getLogger().setLevel(logging.INFO)
+# FORMAT = "%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s"
+# logging.basicConfig(format=FORMAT)
+# logging.getLogger().setLevel(logging.INFO)
 
-__all__ = ["hello", "read_wav_file"]
+__all__ = ["hello", "read_wav_file", "read_wav_file_np"]
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use log::info;
 use hound;
 use pyo3::prelude::*;
 
+use numpy::{IntoPyArray, PyArray1};
 // a general pattern would be implemented pure rust functions
 // then wrap as python functions by using pyo3's macros
 
@@ -18,7 +19,7 @@ fn py_hello() -> PyResult<String> {
 }
 
 /// Reads a WAV file and returns the samples as a Vec<i16> wrapped with PyResult.
-#[pyfunction]
+#[pyfunction(name = "read_wav_file")]
 fn read_wav_file(file_path: &str) -> PyResult<Vec<i16>> {
     info!("Reading WAV file: {}", file_path);
     let mut reader = hound::WavReader::open(file_path).unwrap();
@@ -27,12 +28,21 @@ fn read_wav_file(file_path: &str) -> PyResult<Vec<i16>> {
     Ok(samples)
 }
 
+#[pyfunction]
+fn read_wav_file_np<'py>(py: Python<'py>, file_path: &str) -> PyResult<Bound<'py, PyArray1<i16>>> {
+    info!("Reading WAV file: {}", file_path);
+    let mut reader = hound::WavReader::open(file_path).unwrap();
+    info!("reader created");
+    let samples: Vec<i16> = reader.samples::<i16>().map(|s| s.unwrap()).collect();
+    Ok(samples.into_pyarray_bound(py))
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _lowlevel(_py: Python, m: &PyModule) -> PyResult<()> {
-    pyo3_log::init();
     m.add_function(wrap_pyfunction!(py_hello, m)?)?;
     m.add_function(wrap_pyfunction!(read_wav_file, m)?)?;
+    m.add_function(wrap_pyfunction!(read_wav_file_np, m)?)?;
     Ok(())
 }
 
