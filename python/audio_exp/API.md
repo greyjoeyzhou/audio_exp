3579dc300da961ee180c7f9aee32a0d4686b5d42
Tom Gou <liltom.eth@gmail.com>
2024-04-22 01:46:49 -0700

    add AudioSegment functions

--- python/audio_exp/API.md
+++ python/audio_exp/API.md
@@ -8,6 +8,34 @@ The WAV audio file format is a binary format that exhibits the following structu
 
 As you can see, a WAV file begins with a **header** comprised of metadata, which describes how to interpret the sequence of **audio frames** that follow. Each frame consists of **channels** that correspond to loudspeakers, such as left and right or front and rear.
 
+Python’s `wave` module supports only the **Pulse-Code Modulation (PCM)** encoding.
+
+Moreover, Python is limited to **integer data types**, while PCM doesn’t stop there, defining several bit depths to choose from, including [floating-point](https://realpython.com/python-numbers/#floating-point-numbers) ones:
+
+| Data Type      | Signed | Bits | Min Value          | Max Value         |
+| -------------- | ------ | ---- | ------------------ | ----------------- |
+| Integer        | No     | 8    | 0                  | 255               |
+| Integer        | Yes    | 16   | -32,768            | 32,767            |
+| Integer        | Yes    | 24   | -8,388,608         | 8,388,607         |
+| Integer        | Yes    | 32   | -2,147,483,648     | 2,147,483,647     |
+| Floating-Point | Yes    | 32   | ≈ -3.40282 × 1038  | ≈ 3.40282 × 1038  |
+| Floating-Point | Yes    | 64   | ≈ -1.79769 × 10308 | ≈ 1.79769 × 10308 |
+
+The 8-bit, 16-bit, and 32-bit integers have standard representations in the [C programming language](https://realpython.com/c-for-python-programmers/), which the default [CPython](https://realpython.com/cpython-source-code-guide/) interpreter builds on. However, the 24-bit integer is an outlier without a corresponding built-in **C data type**. 
+
+The `wave` module only supports four integer-based, uncompressed **PCM encoding** bit depths:
+
+- 8-bit unsigned integer
+- 16-bit signed integer
+- 24-bit signed integer
+- 32-bit signed integer
+
+The underlying **audio frames** get exposed to you as an unprocessed [`bytes`](https://realpython.com/python-strings/#bytes-objects) instance, which is a really long sequence of **unsigned byte** values. 
+
+Unfortunately, you can’t do much beyond what you’ve seen here because the `wave` module merely returns the raw bytes without providing any help in their interpretation.
+
+
+
 ## Open / AudioReader
 
 ### wave like read/write
@@ -93,18 +121,15 @@ Args:
 
 `wave` in python containing these metadata: `_wave_params(nchannels=1, sampwidth=2, framerate=16000, nframes=2648832, comptype='NONE', compname='not compressed')`
 
+`pydub` store `self._data = data` as `bytes`.
+
+
+
 ### from_file()
 
 keep this method to read the whole audio into an `AudioSegment`
 
-More advanced read can use `anyaudio.open` like:
-
-```
-```
-
-
-
-
+More advanced read can use `anyaudio.open`
 
 ### export()
 
--- python/audio_exp/audio_segment.py
+++ python/audio_exp/audio_segment.py
@@ -1,4 +1,6 @@
 import array
+import wave
+
 import numpy as np
 
 
@@ -8,153 +10,60 @@ class AudioSegment:
         Initialize an AudioSegment object.
 
         Args:
-            data (array.array or bytes): The audio data.
+            data (array.array or bytes or ndarray): The audio data.
+            channels (int): The number of audio channels.
             sample_width (int): The sample width in bytes.
             frame_rate (int): The frame rate in Hz.
-            channels (int): The number of audio channels.
-
-        Raises:
-            Exception: If either all audio parameters or no parameter is specified.
-            ValueError: If the data length is not a multiple of '(sample_width * channels)'.
-
-        Notes:
-            - If `data` is an `array.array` object, it will be converted to bytes.
-            - If any of the audio parameters (`sample_width`, `frame_rate`, `channels`) are specified,
-              all of them must be specified.
-            - If `sample_width` is specified, the data length must be a multiple of `(sample_width * channels)`.
-            - If `sample_width` is not specified, the `frame_width` will not be calculated and `_data` will be None.
-            - TODO: Convert 24-bit audio to 32-bit audio.
-              (stdlib audioop and array modules do not support 24-bit data)
         """
+        self.channels = kwargs.pop("channels", None)
         self.sample_width = kwargs.pop("sample_width", None)
         self.frame_rate = kwargs.pop("frame_rate", None)
-        self.channels = kwargs.pop("channels", None)
 
-        audio_params = (self.sample_width, self.frame_rate, self.channels)
-
-        # data here maybe the raw wave frames without the header
-        if isinstance(data, array.array):
-            try:
-                data = data.tobytes()
-            except:
-                data = data.tostring()
-        # numpy array?
-        # if isinstance(data, np.array):
-        #     try:
-        #         data = data.tobytes()
-        #     except:
-        #         data = data.tostring()
-
-        # wav object?
-        # normal construction
-        try:
-            data = data if isinstance(data, (str, bytes)) else data.read()
-        except OSError:
-            d = b""
-            reader = data.read(2**31 - 1)
-            while reader:
-                d += reader
-                reader = data.read(2**31 - 1)
-            data = d
-
-        wav_data = read_wav_audio(data)
-        if not wav_data:
-            raise Exception("Couldn't read wav audio from data")
-
-        self.channels = wav_data.channels
-        self.sample_width = wav_data.bits_per_sample // 8
-        self.frame_rate = wav_data.sample_rate
         self.frame_width = self.channels * self.sample_width
-        self._data = wav_data.raw_data
 
-        # convert from unsigned integers in wav
-        # try:
-        #     import audioop
-        # except ImportError:
-        #     import pyaudioop as audioop
-        # if self.sample_width == 1:
-        #     self._data = audioop.bias(self._data, 1, -128)
+        if isinstance(data, array.array):
+            data = data.tobytes()
 
-        # prevent partial specification of arguments
-        if any(audio_params) and None in audio_params:
-            raise Exception(
-                "Either all audio parameters or no parameter must be specified"
-            )
-        # all arguments are given
-        elif self.sample_width is not None:
-            if len(data) % (self.sample_width * self.channels) != 0:
-                raise ValueError(
-                    "data length must be a multiple of '(sample_width * channels)'"
-                )
+        elif isinstance(data, bytes):
+            # The less-than symbol (<) in the format string explicitly
+            # indicates little-endian as the byte order of
+            # each two-byte audio sample (h).
+            data = np.frombuffer(data, dtype="<h")
+        elif isinstance(data, np.ndarray):
+            pass
 
-            self.frame_width = self.channels * self.sample_width
-
-        # TODO
-        # Convert 24-bit audio to 32-bit audio.
-        # (stdlib audioop and array modules do not support 24-bit data)
+        self._data = data
 
     @classmethod
     def from_file(cls, file_path):
-        # Add code to read audio data from a file
-        pass
+        # read audio data from a file into AudioSegment object
+        # Tmperarily use wave module to read audio data
+
+        with wave.open(file_path) as wav_file:
+            metadata = wav_file.getparams()
+
+            # the wave module merely returns the raw bytes
+            # without providing any help in their interpretation.
+            # temporarily read all frames
+            frames = wav_file.readframes(metadata.nframes)
+
+        obj = cls(
+            data=frames,
+            sample_width=metadata.sampwidth,
+            frame_rate=metadata.framerate,
+            channels=metadata.nchannels,
+        )
+        return obj
 
     def export(self, output_file_path):
-        # Add code to export the audio segment to a new file
-        pass
-
-    def __len__(self):
-        """
-        returns the length of this audio segment in milliseconds
-        """
-        pass
-        # return round(1000 * (self.frame_count() / self.frame_rate))
-
-    def __add__(self, arg):
-        pass
-        # if isinstance(arg, AudioSegment):
-        #     return self.append(arg, crossfade=0)
-        # else:
-        #     return self.apply_gain(arg)
-
-    def __sub__(self, arg):
-        pass
-        # if isinstance(arg, AudioSegment):
-        #     raise TypeError(
-        #         "AudioSegment objects can't be subtracted from " "each other"
-        #     )
-        # else:
-        #     return self.apply_gain(-arg)
-
-    def __mul__(self, arg):
-        """
-        If the argument is an AudioSegment, overlay the multiplied audio
-        segment.
-
-        If it's a number, just use the string multiply operation to repeat the
-        audio.
-
-        The following would return an AudioSegment that contains the
-        audio of audio_seg eight times
-
-        `audio_seg * 8`
-        """
-        pass
-        # if isinstance(arg, AudioSegment):
-        #     return self.overlay(arg, position=0, loop=True)
-        # else:
-        #     return self._spawn(data=self._data * arg)
-
-    def __radd__(self, rarg):
-        """
-        Permit use of sum() builtin with an iterable of AudioSegments
-        """
-        pass
-        # if rarg == 0:
-        #     return self
-        # raise TypeError("Gains must be the second addend after the " "AudioSegment")
-
-    def __iter__(self):
-        pass
-        # return (self[i] for i in xrange(len(self)))
+        # export the AudioSegment to a new file
+        # writing in binary mode
+        with wave.open(output_file_path, mode="wb") as wav_file:
+            wav_file.setnchannels(self.channels)
+            wav_file.setsampwidth(self.sample_width)
+            wav_file.setframerate(self.frame_rate)
+            # writeframes() takes a bytes object as input
+            # is there any overhead in converting the ndarray data to bytes?
+            wav_file.writeframes(self._data)
 
     # Add more methods as needed
