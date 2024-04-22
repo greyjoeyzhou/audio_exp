272dfab962cfa0c5dc6c746794b182e4f72821ba
Tom Gou <liltom.eth@gmail.com>
2024-04-22 02:24:01 -0700

    add wav pcm decode

--- python/audio_exp/audio_segment.py
+++ python/audio_exp/audio_segment.py
@@ -3,6 +3,57 @@ import wave
 
 import numpy as np
 
+from enum import IntEnum
+
+
+class PCMEncoding(IntEnum):
+    UNSIGNED_8 = 1
+    SIGNED_16 = 2
+    SIGNED_24 = 3
+    SIGNED_32 = 4
+
+    @property
+    def max(self):
+        return 255 if self == 1 else -self.min - 1
+
+    @property
+    def min(self):
+        return 0 if self == 1 else -(2 ** (self.num_bits - 1))
+
+    @property
+    def num_bits(self):
+        return 8 * self
+
+    def decode(self, frames):
+        match self:
+            case PCMEncoding.UNSIGNED_8:
+                # return np.frombuffer(frames, "u1") / self.max * 2 - 1
+                return np.frombuffer(frames, "u1")
+            case PCMEncoding.SIGNED_16:
+                # The less-than symbol (<) in the format string explicitly
+                # indicates little-endian as the byte order of
+                # each two-byte audio sample (i2).
+                # return np.frombuffer(frames, "<i2") / -self.min
+                return np.frombuffer(frames, "<i2")
+            case PCMEncoding.SIGNED_24:
+                triplets = np.frombuffer(frames, "u1").reshape(-1, 3)
+                padded = np.pad(triplets, ((0, 0), (0, 1)), mode="constant")
+                samples = padded.flatten().view("<i4")
+                samples[samples > self.max] += 2 * self.min
+                return samples
+
+                # The following code is an alternative less efficient implementation
+                # samples = (
+                #     int.from_bytes(frames[i : i + 3], byteorder="little", signed=True)
+                #     for i in range(0, len(frames), 3)
+                # )
+                # return np.fromiter(samples, "<i4")
+            case PCMEncoding.SIGNED_32:
+                # return np.frombuffer(frames, "<i4") / -self.min
+                return np.frombuffer(frames, "<i4")
+            case _:
+                raise TypeError("unsupported encoding")
+
 
 class AudioSegment:
     def __init__(self, data=None, *args, **kwargs):
@@ -21,14 +72,16 @@ class AudioSegment:
 
         self.frame_width = self.channels * self.sample_width
 
+        pcm = PCMEncoding(self.sample_width)
+
+        # array aleady has type information
+        # TODO maybe remove support for array.array
         if isinstance(data, array.array):
-            data = data.tobytes()
+            data = np.array(data)
 
         elif isinstance(data, bytes):
-            # The less-than symbol (<) in the format string explicitly
-            # indicates little-endian as the byte order of
-            # each two-byte audio sample (h).
-            data = np.frombuffer(data, dtype="<h")
+            data = pcm.decode(data)
+            # data = np.frombuffer(data, dtype="<h")
         elif isinstance(data, np.ndarray):
             pass
 
