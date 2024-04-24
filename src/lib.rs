650bdf8dfef7f5118c4fae17de47f22f2f4e816b
joeyzhou1984 <joeyzhou1984@gmail.com>
2024-04-23 23:49:43 -0700

    seek then read

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use pyo3::exceptions::PyValueError;
+
 use log::info;
 
 use hound;
@@ -112,12 +114,26 @@ fn read_wav_file_metadata(file_path: &str) -> PyResult<WavFileMeta> {
 }
 
 #[pyfunction]
-fn read_wav_file_np<'py>(py: Python<'py>, file_path: &str) -> PyResult<Bound<'py, PyArray1<i16>>> {
+fn read_wav_file_np<'py>(
+    py: Python<'py>,
+    file_path: &str,
+    starting_time_ms: u32,
+) -> PyResult<Bound<'py, PyArray1<i16>>> {
     info!("Reading WAV file: {}", file_path);
     let mut reader = hound::WavReader::open(file_path).unwrap();
     info!("reader created");
-    let samples: Vec<i16> = reader.samples::<i16>().map(|s| s.unwrap()).collect();
-    Ok(samples.into_pyarray_bound(py))
+    let duration = reader.duration();
+    let sample_rate = reader.spec().sample_rate;
+    let starting_sample = starting_time_ms / 1000 * sample_rate;
+    if starting_sample > duration {
+        Err(PyErr::new::<PyValueError, _>(
+            "Starting sample is beyond the duration of the WAV file",
+        ))
+    } else {
+        reader.seek(starting_sample).unwrap();
+        let samples: Vec<i16> = reader.samples::<i16>().map(|s| s.unwrap()).collect();
+        Ok(samples.into_pyarray_bound(py))
+    }
 }
 
 /*
