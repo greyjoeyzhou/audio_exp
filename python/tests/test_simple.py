c5fbce53429ab0cfeecf403251e6fe484399b0d7
joeyzhou1984 <joeyzhou1984@gmail.com>
2024-06-23 15:56:21 -0700

    add type hint and some tests in python side

--- python/tests/test_simple.py
+++ python/tests/test_simple.py
@@ -1,8 +1,34 @@
 import os
-from audio_exp.audio_segment import AudioSegment
 
-TEST_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sounds")
+from audio_exp.audio_segment import (
+    AudioSegment,
+    read_wav_file_metadata,
+    read_wav_file_np,
+)
 
-file_path = os.path.join(TEST_DIR, "StarWars3.wav")
-as1 = AudioSegment.from_file(file_path)
-print(as1)
+from .common import TEST_DIR
+
+
+def test_read_wav_file_single_channel():
+    file_path = os.path.join(TEST_DIR, "StarWars3.wav")
+    as1 = AudioSegment.from_file(file_path)
+    as2 = read_wav_file_np(file_path, 0)
+    tmeta = read_wav_file_metadata(file_path)
+    assert len(as1._data) == len(as2)
+    print(tmeta)
+
+
+def test_read_wav_file_stereo_channel():
+    file_path = os.path.join(TEST_DIR, "44100_pcm16_stereo.wav")
+    as1 = AudioSegment.from_file(file_path)
+
+    as2 = read_wav_file_np(file_path, 0)
+    assert len(as1._data) == len(as2)
+
+    tmeta = read_wav_file_metadata(file_path)
+    assert as1.channels == tmeta.channels
+
+
+# TODO
+# surround sound
+# pcm08, pcm24, pcm32
