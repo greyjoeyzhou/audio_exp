efe9c414b654f98101973a6a541f680f4ea5c384
Tom <liltom.eth@gmail.com>
2024-06-23 15:16:31 -0700

    add rs audio read in audio_segment

--- python/tests/test_simple.py
+++ python/tests/test_simple.py
@@ -1,8 +1,10 @@
 import os
 from audio_exp.audio_segment import AudioSegment
+from audio_exp import read_wav_file_np
 
 TEST_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sounds")
 
 file_path = os.path.join(TEST_DIR, "StarWars3.wav")
 as1 = AudioSegment.from_file(file_path)
-print(as1)
+as2 = read_wav_file_np(file_path, 0)
+assert len(as1._data) == len(as2)
