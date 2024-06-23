c9ffe312ff741b6ca8476619fda43bf3ff164753
joeyzhou1984 <joeyzhou1984@gmail.com>
2024-06-23 13:27:09 -0700

    reorg test data

--- python/tests/test_simple.py
+++ python/tests/test_simple.py
@@ -1,9 +1,8 @@
 import os
 from audio_exp.audio_segment import AudioSegment
 
-file_path = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data/StarWars3.wav"
-)
+TEST_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sounds")
 
+file_path = os.path.join(TEST_DIR, "StarWars3.wav")
 as1 = AudioSegment.from_file(file_path)
 print(as1)
